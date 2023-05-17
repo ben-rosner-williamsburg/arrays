f4866df29ecd11a95b387aaf22b0456ea84cf061
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-17 17:13:27 -0400

    Add array methods with explanations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var stringArr = ["I", "am", "an", "array"];
 var numberArr = [1, 2, 3, 4];
 var booleanArr = [true, false, true, true];
+
+//This method will add the strings "of" and "strings" to the end of the stringArr.
+stringArr.push("of", "strings");
+
+//This method will remove the 4 from the numberArr
+numberArr.pop();
+
+//This method will create a new array with just the first two values (true, false)
+booleanArr.slice(0, 2);
