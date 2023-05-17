c64f85f809a9a27a0192fb952b0bf485b2e4d41f
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-17 17:16:05 -0400

    Add index positioning explanation

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ numberArr.pop();
 
 //This method will create a new array with just the first two values (true, false)
 booleanArr.slice(0, 2);
+
+/* The first index position of an array is 0. 
+We then count upwards from there.  So the 2nd value 
+of an array is at index 1, the third value is at index 2, etc. */
